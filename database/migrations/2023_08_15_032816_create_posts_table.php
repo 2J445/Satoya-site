10417a3f8a1b3d5af38ef3d4984f24243500da66
EC2 Default User <ec2-user@ip-172-31-43-26.ap-northeast-1.compute.internal>
2023-08-22 02:58:50 +0000

    mysite-toppage-login

---
+++ database/migrations/2023_08_15_032816_create_posts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tests', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('user_id');
+            $table->string('name');
+            $table->string('image');
+            $table->string('old');
+            $table->string('gender');
+            $table->text('explanation');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tests');
+    }
+};
